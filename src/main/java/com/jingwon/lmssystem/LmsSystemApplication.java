236eb24912684c3e350165fc79e4d5f4bd767625
jingwon.lee <nesaz0522@naver.com>
2023-04-20 14:40:45 +0900

    refactor project name

--- src/main/java/com/jingwon/lmssystem/LmsSystemApplication.java
+++ src/main/java/com/jingwon/lmssystem/LmsSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LmssystemApplication {
+public class LmsSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LmssystemApplication.class, args);
+        SpringApplication.run(LmsSystemApplication.class, args);
     }
 
 }
