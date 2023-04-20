a4315d39ede1361d46364ed498894db0c8d9ca81
jingwon.lee <nesaz0522@naver.com>
2023-04-20 14:37:14 +0900

    ignore  추가

---
+++ src/main/java/com/jingwon/lmssystem/LmssystemApplication.java
@@ -0,0 +1,13 @@
+package com.jingwon.lmssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jingwon/lmssystem/LmssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingwon.lmssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
